9fcc2362421141d1ee46f2f952e22cb511e00b9c
Jeffrey Paarhuis <jpaarhuis@home.nl>
2019-03-22 17:32:05 +0100

    Werkende function

---
+++ PizzabodemFunction/ToevoegenPizzabodem.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PizzabodemFunction
+{
+    public static class ToevoegenPizzabodem
+    {
+        [FunctionName("toevoegen-pizzabodem")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Adding pizzabodem to pizza");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var pizza = JsonConvert.DeserializeObject<Pizza.Models.Pizza>(requestBody);
+
+            pizza.Ingredienten.Add("pizzabodem");
+            
+            return new OkObjectResult(pizza);
+        }
+    }
+}
